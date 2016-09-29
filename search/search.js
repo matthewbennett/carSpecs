f6840238765323923486cb7e73a36a943bf8bf9c
matthew <matthew@matthews-Mac-mini-2.local>
2016-09-29 10:57:24 -0400

    finished search

--- search/search.js
+++ search/search.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
 
     var edmundsApiKey = 'jdq9c2drnq3nqqwpcek7ynjv';
     var enmundsBaseUrl = 'https://api.edmunds.com/api/vehicle/v2/';
+    var makes = {};
+    var year = '2016';
 
     function fullscreen() {
         $('a').click(function() {
@@ -17,62 +19,78 @@ $(document).ready(function() {
 
     var onPageLoad = {
         init: function() {
+            this.getYears();
             this.getMakes();
         },
 
         getMakes: function() {
-            $.get(enmundsBaseUrl + "makes?state=used&year=2016&view=basic&fmt=json&api_key=" + edmundsApiKey, function(data) {
+            $.get(enmundsBaseUrl + "makes?state=used&year=" + year+"&view=basic&fmt=json&api_key=" + edmundsApiKey, function(data) {
                 console.log(data);
-                // data.makes.splice(1, 1);
-                // data.makes.splice(37, 1);
-                // data.makes.splice(29, 1);
-                // data.makes.splice(20, 1);
+                makes = data.makes;
+                populateMakes(data);
+                $('.makePoint:eq(0)').trigger('click');
             });
+        },
+        getYears: function() {
+            for (var i = 1990; i < 2017; i++) {
+                $('#yearDrop').prepend('<li class="yearPoint"><a href="#">' + i + '</a></li>');
+            }
         }
     };
 
     var userInteractions = {
         init: function() {
-            this.showCars();
-            this.viewCarDetials();
+            this.setMake();
+            this.setModel();
+            this.setYear();
         },
-
-        showCars: function() {
-            $(document).on('mouseenter', '.carMakeSpecsContainer', function() {
-                $(this).find('.makeCarsList').show();
-            });
-            $(document).on('mouseleave', '.carMakeSpecsContainer', function() {
-                $(this).find('.makeCarsList').hide();
+        setMake: function() {
+            $(document).on('click', '.makePoint', function() {
+                var currentMake = $(this).text();
+                $('.makeBtn').empty();
+                $('.makeBtn').append(currentMake + '<span class="caret"></span>');
+                populateModels(currentMake);
             });
         },
-        viewCarDetials: function() {
-            $(document).on('click', '.carNameContainer', function() {
-                var engineID = null;
-                var carID = $(this).attr('id');
-                var makeID = $(this).parent().attr('id');
-                $('.carTitle').text('');
-                $(this).parent().parent().find('.carTitle').text(carID);
-                $.get(enmundsBaseUrl + makeID + '/' + carID + '?state=new&year=2014&view=basic&fmt=json&api_key=' + edmundsApiKey, function(data) {
-                    engineID = data.years[0].styles[0].id;
-                    $.get(enmundsBaseUrl + 'styles/' + engineID + '/engines?availability=standard&fmt=json&api_key=' + edmundsApiKey, function(data) {
-                        console.log(data);
-                        var specs = Handlebars.compile($('#specInfo').html());
-                        var specData = specs(data.engines[0]);
-                        $('.specList').empty();
-                        $('.specList').append(specData);
-                    });
-                    $.get('https://api.edmunds.com/v1/api/tco/newtruecosttoownbystyleidandzip/' + engineID + '/30341?fmt=json&api_key=' + edmundsApiKey, function(data) {
-                        console.log(data);
-                    });
-                });
-                $('.carSpecDropdown').hide();
-                $('#drop' + makeID).show();
+        setModel: function() {
+            $(document).on('click', '.modelPoint', function() {
+                var currentMake = $(this).text();
+                $('.modelBtn').empty();
+                $('.modelBtn').append(currentMake + '<span class="caret"></span>');
+                populateModels(currentMake);
+            });
+        },
+        setYear: function() {
+            $(document).on('click', '.yearPoint', function() {
+                var currentYear= $(this).text();
+                year = currentYear;
+                $('.yearBtn').empty();
+                $('.yearBtn').append(currentYear + '<span class="caret"></span>');
+                onPageLoad.getMakes();
+                //populateModels(currentMake);
             });
         }
     };
 
+    function populateMakes(data) {
+        $('#makeDrop').empty();
+        $.each(data.makes, function(ind, item) {
+            $('#makeDrop').append('<li class="makePoint"><a href="#">' + item.name + '</a></li>')
+        });
+    };
 
-
+    function populateModels(currentMake) {
+        $.each(makes, function(ind, item) {
+            if (item.name == currentMake) {
+                $('.modelBtn').empty();
+                $('.modelBtn').append(item.models[0].name + '<span class="caret"></span>');
+                $('#modelDrop').empty();
+                $.each(item.models, function(index, mod) {
+                    $('#modelDrop').append('<li class="modelPoint"><a href="#">' + mod.name + '</a></li>');
+                });
+            }
+        });
+    };
 
     (function() {
         onPageLoad.init();
